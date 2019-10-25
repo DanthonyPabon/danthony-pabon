1f43fe8be118ae1dae447b4eee9a5bab48452e72
Danthony Pabon <danthonypabong@gmail.com>
2019-10-25 12:30:47 -0400

    [NPM] Libraries necessaries  📚
    * "react-native-animatable": "^1.3.3"
    * "react-native-fast-image": "^6.0.0"
    * "react-native-infinite-scroll-grid": "^0.1.0"
    *  "react-native-linear-gradient": "^2.5.6"
    * "react-native-vector-icons": "^6.6.0"
    *  "react-navigation-fluid-transitions": "^0.3.2"

--- android/app/src/main/java/com/danthonypabon/MainApplication.java
+++ android/app/src/main/java/com/danthonypabon/MainApplication.java
@@ -3,6 +3,9 @@ package com.danthonypabon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new FastImageViewPackage(),
+            new LinearGradientPackage(),
             new RNGestureHandlerPackage()
       );
     }
